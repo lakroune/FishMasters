2fc831d0596b694e6dfd526a2210591d2157189f
lakroune <lakroune00@gmail.com>
2026-01-20 20:35:51 +0100

    refactor: optimize User model by consolidating methods and enhancing performance

---
+++ app/models/Pecheur.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\models;
+
+use Exception;
+
+class   Pecheur extends User
+{
+    private string $photo_pecheur;
+    private string $region;
+    private string $type_peche_favorite;
+    private int $id_equipe;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function __toString()
+    {
+        return parent::__toString() . "pecheur : photo_pecheur = $this->photo_pecheur, region = $this->region, type_peche_favorite = $this->type_peche_favorite, id_equipe = $this->id_equipe";
+    }
+
+    public function getPhotoPecheur(): string
+    {
+        return $this->photo_pecheur;
+    }
+
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    public function getTypePeche(): string
+    {
+        return $this->type_peche_favorite;
+    }
+
+    public function getIdEquipe(): int
+    {
+        return $this->id_equipe;
+    }
+
+    public function setPhotoPecheur(string $photo): void
+    {
+        if (empty($photo)) {
+            throw new Exception("La photo de l'équipe ne doit pas être vide");
+        }
+
+        $this->photo_pecheur = $photo;
+    }
+
+    public function setRegion(string $region): void
+    {
+        if (empty($region)) {
+            throw new Exception("La region de l'équipe ne doit pas être vide");
+        }
+
+        $this->region = $region;
+    }
+
+    public function setTypePeche(string $type): void
+    {
+        if (empty($type)) {
+            throw new Exception("Le type de l'équipe ne doit pas être vide");
+        }
+
+        $this->type_peche_favorite = $type;
+    }
+
+    public function setIdEquipe(int $id): void
+    {
+        if ($id < 0) {
+            throw new Exception("L'id de l'équipe doit être supérieur à 0");
+        }
+
+        $this->id_equipe = $id;
+    }
+    
+}
