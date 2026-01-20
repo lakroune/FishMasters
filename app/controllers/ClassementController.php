22be277255f41910c054483b9891ed9e043628a4
lakroune <lakroune00@gmail.com>
2026-01-20 21:49:37 +0100

    chore: remove unused test.html file to clean up the project structure

---
+++ app/controllers/ClassementController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\controllers;
+
+class ClassementController
+{
+    public function index()
+    {
+        require_once __DIR__ . '/../views/classement.php';
+    }
+    public function default()
+    {
+        require_once __DIR__ . '/../views/classement.php';
+    }
+    public function mer()
+    {
+        require_once __DIR__ . '/../views/classement.php';
+    }
+    public function eaudouce()
+    {
+        require_once __DIR__ . '/../views/classement.php';
+    }
+    
+}
