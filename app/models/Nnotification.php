a593cca9f3ca75848a17ad363581c6dcc6bc3651
Mouad Gourita <mouad.gourita@gmail.com>
2026-01-21 11:24:50 +0100

    feat: added Notification Class

---
+++ app/models/Nnotification.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\models;
+
+Class Nnotification
+{
+    private $id_note;
+    private $contenu;
+    private $date;
+
+    public function __set($proprty, $value)
+    {
+        $this->$proprty = $value;
+    }
+
+    public function __get($proprty)
+    {
+        return $this->$proprty;
+    }
+
+}
+
+?>
