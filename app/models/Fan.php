5058ee597d510dca204cb51324e7904810b1a4e7
lakroune <lakroune00@gmail.com>
2026-01-21 12:29:16 +0100

    feat: enhance Fan model with registration method and data population functionality

--- app/models/Fan.php
+++ app/models/Fan.php
@@ -2,11 +2,48 @@
 
 namespace app\models;
 
+use app\model\Connexion;
+use Exception;
+
 class Fan extends User
 {
-    public function __construct() {}
+    public function __construct()
+    {
+        parent::__construct();
+    }
     public function __toString()
     {
         return parent::__toString();
+    }
+    public function register(array $data): bool
+    {
+        $db = Connexion::connect()->getConnexion();
+        $query = "INSERT INTO user (nom_user, prenom_user, email, password_user, role_user) VALUES (:nom_user, :prenom_user, :email, :password_user, :role_user)";
+        try {
+            $stmt = $db->prepare($query);
+        } catch (Exception $e) {
+            throw new Exception("Une erreur est survenue lors de la requête SQL : " . $query . " : " . $e->getMessage());
+        }
+        $this->rempirer($data);
+        $stmt->bindValue(':nom_user', $this->nom_user);
+        $stmt->bindValue(':prenom_user', $this->prenom_user);
+        $stmt->bindValue(':email', $this->email);
+        $stmt->bindValue(':password_user', $this->password_user);
+        $stmt->bindValue(':role_user', $this->role_user);
+        return $stmt->execute();
+    }
+    /**
+     * Remplit les valeurs d'un tableau dans les attributs de l'objet
+     * en appelant les méthodes setter correspondantes.
+     * @param array $data tableau contenant les valeurs à rempiler
+     */
+    private function rempirer(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
     }
 }
