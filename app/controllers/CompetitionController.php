5db20192d640565d8a46e66b645d9f96b704bab6
Mouad Gourita <mouad.gourita@gmail.com>
2026-01-22 09:15:28 +0100

    feat: implement CompetitionController with index and addComptition methods

---
+++ app/controllers/CompetitionController.php
@@ -0,0 +1,21 @@
+<?php
+    namespace app\controllers;
+
+
+
+Class CompetitionController
+{
+
+    public function index()
+    {
+        require_once __DIR__ . '/../views/addCompetition.php';
+    }
+    
+    public function addComptition()
+    {
+
+    }
+
+}
+
+?>
