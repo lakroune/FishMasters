93704ab94c445a58108e1a857be819d0511861b5
lakroune <lakroune00@gmail.com>
2026-01-22 21:17:20 +0100

    refactor: remove unnecessary require_once statement from Classement model

--- app/models/Classement.php
+++ app/models/Classement.php
@@ -2,7 +2,6 @@
 
 namespace app\models;
 
-require_once  '../../config/Connexion.php';
 
 use config\Connexion;
 use Exception;
@@ -200,4 +199,3 @@ class Classement
         return $stmt->fetchAll(PDO::FETCH_CLASS, Classement::class);
     }
 }
-
