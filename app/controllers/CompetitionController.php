3410313ace6032063b3355af23695218b9e8075c
Mouad Gourita <mouad.gourita@gmail.com>
2026-01-26 21:42:39 +0100

    feat: update database connection settings for production environment

--- app/controllers/CompetitionController.php
+++ app/controllers/CompetitionController.php
@@ -31,11 +31,8 @@ class CompetitionController
 
         require_once __DIR__ . '/../views/competitions.php';
     }
-<<<<<<< HEAD
-=======
     public function default()
     {
         $this->index();
     }
->>>>>>> a711dbfee3ebe1517376038b7f2265ef7fd3a2cc
 }
